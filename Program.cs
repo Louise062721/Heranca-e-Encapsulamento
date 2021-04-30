3c3e99f3cc93d80a59374c862945498c1f3f7d4d
Louise062721 <82733024+Louise062721@users.noreply.github.com>
2021-04-30 00:38:46 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,50 +14,43 @@ namespace Herança_e_encapsulamento_1
             Retangulo ret1 = new Retangulo(2.0, 7.8);
 
             Console.WriteLine();
-            Console.WriteLine("Altura:\t\t{0:0.0}", ret1.Altura);
-            Console.WriteLine("Largura:\t{0:0.0}", ret1.Largura);
-            Console.WriteLine("Área:\t\t{0:0.0}", ret1.Area);
-            Console.WriteLine("Perímetro\t{0:0.0}", ret1.Perimetro);
+            Console.WriteLine(ret1.Imprimir());
+           
 
             Console.WriteLine();
             ret1.Altura = 27.9;
             ret1.Largura = 2.4;
 
             Console.WriteLine();
-            Console.WriteLine("Altura:\t\t{0:0.0}", ret1.Altura);
-            Console.WriteLine("Largura:\t{0:0.0}", ret1.Largura);
-            Console.WriteLine("Área:\t\t{0:0.0}", ret1.Area);
-            Console.WriteLine("Perímetro\t{0:0.0}", ret1.Perimetro);
+            Console.WriteLine(ret1.Imprimir());
 
             Console.WriteLine();
             ret1.Altura = -87.8;
             ret1.Largura = 29.4;
 
             Console.WriteLine();
-            Console.WriteLine("Altura:\t\t{0:0.0}", ret1.Altura);
-            Console.WriteLine("Largura:\t{0:0.0}", ret1.Largura);
-            Console.WriteLine("Área:\t\t{0:0.0}", ret1.Area);
-            Console.WriteLine("Perímetro\t{0:0.0}", ret1.Perimetro);
+            Console.WriteLine(ret1.Imprimir());
 
             Console.WriteLine();
             ret1.Altura = 2.6;
             ret1.Largura = -22.5;
 
             Console.WriteLine();
-            Console.WriteLine("Altura:\t\t{0:0.0}", ret1.Altura);
-            Console.WriteLine("Largura:\t{0:0.0}", ret1.Largura);
-            Console.WriteLine("Área:\t\t{0:0.0}", ret1.Area);
-            Console.WriteLine("Perímetro\t{0:0.0}", ret1.Perimetro);
+            Console.WriteLine(ret1.Imprimir());
 
             Console.WriteLine();
             ret1.Altura = 12.3;
             ret1.Largura = 31.2;
 
             Console.WriteLine();
-            Console.WriteLine("Altura:\t\t{0:0.0}", ret1.Altura);
-            Console.WriteLine("Largura:\t{0:0.0}", ret1.Largura);
-            Console.WriteLine("Área:\t\t{0:0.0}", ret1.Area);
-            Console.WriteLine("Perímetro\t{0:0.0}", ret1.Perimetro);
+            Console.WriteLine(ret1.Imprimir());
+
+            Console.WriteLine();
+            ret1.Altura = 12.3;
+            ret1.Largura = 31.2;
+
+            Console.WriteLine();
+            Console.WriteLine(ret1.Imprimir());
 
             Console.ReadKey();
         }
